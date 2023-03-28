597f4f9d3c050e7b414beef4390c76416412651d
RbenMoise <reubenmoses91@gmail>
2023-03-28 15:16:16 -0700

    Redused lines in the function

--- _printf.c
+++ _printf.c
@@ -4,69 +4,47 @@
 #include <stdarg.h>
 #include "main.h"
 
-/**
- * _printf - emulates original printf
- * @format: list of arguments
- *
- * Return: 0 for SUCCESS
- */
 int _printf(const char *format, ...)
 {
-	int size = 0, count = 0, n, num, i = 0, j = 0;
-	char *s, c, v, *x, u[11];
-	va_list call;
+	va_list args;
+	va_start(args, format);
+	int count = 0;
 
-	va_start(call, format);
-
-	while (format && format[count])
+	while (*format != '\0')
 	{
-		if (format[count] == '%')
+		if (*format == '%')
 		{
-			count++;
-			switch (format[count])
+			format++;
+			if (*format == 'c')
 			{
-				case 's':
-					s = va_arg(call, char*);
-					_string(s);
-					break;
-				case 'c':
-					v = va_arg(call, int);
-					_char(v);
-					break;
-				case '%':
-					v = va_arg(call, int);
-					_char('%');
-					break;
-				case 'i':
-					n = va_arg(call, int);
-					x = malloc(11 * sizeof(char));
-					if (!x)
-					return (-1);
-					while (n != 0)
-					{
-						num = n % 10;
-						u[i++] = num + '0';
-						n /= 10;
-					}
-					u[i++] = '\0';
-					while (i >= 0)
-					{
-						x[j++] = u[i--];
-					}
-					_string(x);
-					free(x);
-					default:
-					break;
+				char c = (char) va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char*);
+				while (*s != '\0')
+				{
+					putchar(*s);
+					s++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
 			}
 		}
 		else
 		{
-			c = format[count];
-			write(STDOUT_FILENO, &c, 1);
-			size++;
+			putchar(*format);
+			count++;
 		}
-		count++;
+		format++;
 	}
-	va_end(call);
-	return (size);
+
+	va_end(args);
+	return (count);
 }
